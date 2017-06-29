82f604ec2c7e0805210385e48592d9e7593ec46c
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-06-30 03:50:50 +0600

    task automation bot

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var restify = require("restify");
 var builder = require("botbuilder");
+var restify = require("restify");
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -7,16 +7,57 @@ server.listen(process.env.port || process.env.PORT || 3978, function() {
   console.log("%s listening to %s", server.name, server.url);
 });
 
-// Create chat connector for communicating with the Bot Framework Service
+// Create chat bot
 var connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-// Listen for messages from users
-server.post("/api/messages", connector.listen());
+const ChangePasswordOption = "Change Password";
+const ResetPasswordOption = "Reset Password";
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function(session) {
-  session.send("You said: %s", session.message.text);
-});
+var bot = new builder.UniversalBot(connector, [
+  session => {
+    builder.Prompts.choice(
+      session,
+      "What do yo want to do today?",
+      [ChangePasswordOption, ResetPasswordOption],
+      { listStyle: builder.ListStyle.button }
+    );
+  },
+  (session, result) => {
+    if (result.response) {
+      switch (result.response.entity) {
+        case ChangePasswordOption:
+          session.send(
+            "This functionality is not yet implemented! Try resetting your password."
+          );
+          session.reset();
+          break;
+        case ResetPasswordOption:
+          session.beginDialog("resetPassword:/");
+          break;
+      }
+    } else {
+      session.send(
+        `I am sorry but I didn't understand that. I need you to select one of the options below`
+      );
+    }
+  },
+  (session, result) => {
+    if (result.resume) {
+      session.send(
+        "You identity was not verified and your password cannot be reset"
+      );
+      session.reset();
+    }
+  }
+]);
+
+//Sub-Dialogs
+bot.library(require("./dialogs/reset-password"));
+
+//Validators
+bot.library(require("./validators"));
+
+server.post("/api/messages", connector.listen());
